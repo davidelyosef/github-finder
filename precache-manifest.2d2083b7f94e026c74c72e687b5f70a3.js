4d4e22ff5a02527a2aec19185d3c2b819e1fcdaa
David el Yosef <davidyf96@gmail.com>
2020-09-11 20:44:57 +0300

    Updates

---
+++ precache-manifest.2d2083b7f94e026c74c72e687b5f70a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da9a2a9d377ed9f1f50ea84b39fc00c7",
+    "url": "/github-finder/index.html"
+  },
+  {
+    "revision": "dce67b971f6d9149ca2e",
+    "url": "/github-finder/static/css/main.c65e6cf7.chunk.css"
+  },
+  {
+    "revision": "eefad7f77c8b5c9bcd53",
+    "url": "/github-finder/static/js/2.0108d4ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-finder/static/js/2.0108d4ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dce67b971f6d9149ca2e",
+    "url": "/github-finder/static/js/main.c689bc78.chunk.js"
+  },
+  {
+    "revision": "d1cec4377029145402db",
+    "url": "/github-finder/static/js/runtime-main.cead24c5.js"
+  }
+]);
