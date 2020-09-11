61d13a18a925067ef63c6fa7e9bbf9c1b92ddfd7
David el Yosef <davidyf96@gmail.com>
2020-09-11 20:34:06 +0300

    first commit

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+const NotFound = () => {
+    return (
+        <Fragment>
+            <h1 className="lead">The page you are looking for does not exist</h1>
+        </Fragment>
+    )
+}
+
+export default NotFound
